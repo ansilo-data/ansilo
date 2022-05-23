7bb7bb9febda23e8c83ed6e635378d5d471d49e1
Elliot Levin <elliotlevin@hotmail.com>
2022-05-23 14:15:29 +0000

    Add postgres extension

---
+++ ansilopg/src/lib.rs
@@ -0,0 +1,32 @@
+use pgx::*;
+
+pg_module_magic!();
+
+#[pg_extern]
+fn hello_ansilopg() -> &'static str {
+    "Hello, ansilopg"
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+#[pg_schema]
+mod tests {
+    use pgx::*;
+
+    #[pg_test]
+    fn test_hello_ansilopg() {
+        assert_eq!("Hello, ansilopg", crate::hello_ansilopg());
+    }
+
+}
+
+#[cfg(test)]
+pub mod pg_test {
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
+
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        // return any postgresql.conf settings that are required for your tests
+        vec![]
+    }
+}
