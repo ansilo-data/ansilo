da1a360afbe958c340dbead8ad7870281bbe33b9
Elliot Levin <elliotlevin@hotmail.com>
2022-05-30 23:04:01 +1000

    thin fonts

--- workbench/src/config/theme.ts
+++ workbench/src/config/theme.ts
@@ -5,6 +5,12 @@ const theme = createTheme({
   palette: {
     mode: "dark",
   },
+  typography: {
+    fontWeightRegular: 100,
+    h6: {
+      fontWeight: 100,
+    },
+  },
 });
 
 export default theme;
