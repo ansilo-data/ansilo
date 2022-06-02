baa70b876697a88219cc648f887c8057819de3f0
Elliot Levin <elliotlevin@hotmail.com>
2022-06-02 10:55:47 +1000

    update cargo/rustup home to efs

--- .devcontainer/build-devcontainer.sh
+++ .devcontainer/build-devcontainer.sh
@@ -1,6 +1,14 @@
 #/bin/bash
 
-docker build -t rustdevconatiner --env-file .env .
-docker tag rustdevcontainer:latest 059160323628.dkr.ecr.ap-southeast-2.amazonaws.com/rust-devcontainer:latest
+set -e
+
+docker build -t rust-devcontainer \
+ --build-arg CARGO_HOME=/workspace/ansilo/.cargo \
+ --build-arg RUSTUP_HOME=/workspace/ansilo/.rustup \
+ .
+
+docker tag rust-devcontainer 059160323628.dkr.ecr.ap-southeast-2.amazonaws.com/rust-devcontainer:latest
+
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 059160323628.dkr.ecr.ap-southeast-2.amazonaws.com
+
 docker push 059160323628.dkr.ecr.ap-southeast-2.amazonaws.com/rust-devcontainer:latest
