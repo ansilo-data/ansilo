ce8e5850d8ee70206bf93cb8a6db926619a5c08f
Elliot Levin <elliotlevin@hotmail.com>
2022-08-15 21:26:18 +1000

    add facls to devcontainer

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -16,6 +16,6 @@ docker build -t ansilo-devcontainer .
 
 docker tag ansilo-devcontainer 635198228996.dkr.ecr.ap-southeast-2.amazonaws.com/devcontainer:latest
 
-aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 635198228996.dkr.ecr.ap-southeast-2.amazonaws.com
+aws ecr get-login-password --profile $AWS_PROFILE --region ap-southeast-2 | docker login --username AWS --password-stdin 635198228996.dkr.ecr.ap-southeast-2.amazonaws.com
 
 docker push 635198228996.dkr.ecr.ap-southeast-2.amazonaws.com/devcontainer:latest
