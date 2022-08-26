fd216723892a288685955e7b813eb72933cb7ed9
Elliot Levin <elliotlevin@hotmail.com>
2022-08-26 06:44:04 +0000

    add where local condition test

--- ansilo-e2e/tests/oracle/t006_select_where/mod.rs
+++ ansilo-e2e/tests/oracle/t006_select_where/mod.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 use ansilo_connectors_base::interface::LoggedQuery;
+use itertools::Itertools;
 use pretty_assertions::assert_eq;
 use serial_test::serial;
 
@@ -87,7 +88,59 @@ fn test_select_where_constant_string_none_matching() {
 
 #[test]
 #[serial]
-fn test_select_where_param() {
+fn test_select_where_param_prepared_statement() {
+    ansilo_logging::init_for_tests();
+    let containers = super::common::start_oracle();
+    super::common::init_oracle_sql(&containers, crate::current_dir!().join("oracle-sql/*.sql"));
+
+    let (instance, mut client) =
+        crate::main::run_instance(crate::current_dir!().join("config.yml"));
+
+    let statement = client
+        .prepare(
+            r#"
+            SELECT * FROM "ANSILO_ADMIN.T006__TEST_TAB"
+            WHERE "NAME" = $1
+            "#,
+        )
+        .unwrap();
+
+    let names = ["Mary", "John"];
+
+    for name in names.iter() {
+        let rows = client.query(&statement, &[name]).unwrap();
+
+        assert_eq!(rows.len(), 1);
+        assert_eq!(rows[0].get::<_, String>(0), name.to_string());
+    }
+
+    assert_eq!(
+        instance.log().get_from_memory().unwrap(),
+        names
+            .iter()
+            .map(|name| (
+                "oracle".to_string(),
+                LoggedQuery::new(
+                    [
+                        r#"SELECT "t1"."NAME" AS "c0" "#,
+                        r#"FROM "ANSILO_ADMIN"."T006__TEST_TAB" "t1" "#,
+                        r#"WHERE (("t1"."NAME") = (?))"#,
+                    ]
+                    .join(""),
+                    vec![format!(
+                        "LoggedParam [index=1, method=setNString, value={}]",
+                        name
+                    )],
+                    None
+                )
+            ))
+            .collect_vec()
+    );
+}
+
+#[test]
+#[serial]
+fn test_select_where_local_condition() {
     ansilo_logging::init_for_tests();
     let containers = super::common::start_oracle();
     super::common::init_oracle_sql(&containers, crate::current_dir!().join("oracle-sql/*.sql"));
@@ -99,14 +152,14 @@ fn test_select_where_param() {
         .query(
             r#"
             SELECT * FROM "ANSILO_ADMIN.T006__TEST_TAB"
-            WHERE "NAME" = $1
+            WHERE MD5("NAME") = MD5('Jane')
             "#,
-            &[&"Mary"],
+            &[],
         )
         .unwrap();
 
     assert_eq!(rows.len(), 1);
-    assert_eq!(rows[0].get::<_, String>(0), "Mary".to_string());
+    assert_eq!(rows[0].get::<_, String>(0), "Jane".to_string());
 
     assert_eq!(
         instance.log().get_from_memory().unwrap(),
@@ -115,11 +168,10 @@ fn test_select_where_param() {
             LoggedQuery::new(
                 [
                     r#"SELECT "t1"."NAME" AS "c0" "#,
-                    r#"FROM "ANSILO_ADMIN"."T006__TEST_TAB" "t1" "#,
-                    r#"WHERE (("t1"."NAME") = (?))"#,
+                    r#"FROM "ANSILO_ADMIN"."T006__TEST_TAB" "t1""#,
                 ]
                 .join(""),
-                vec!["LoggedParam [index=1, method=setNString, value=Mary]".into(),],
+                vec![],
                 None
             )
         )]
