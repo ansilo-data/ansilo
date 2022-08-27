0e5b5f2a2888560711dbf34e568f1a7dc5efd8d6
Ubuntu <ubuntu@ip-10-0-0-251.ap-southeast-2.compute.internal>
2022-08-27 08:26:48 +0000

    fix devcontainer build script

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -4,18 +4,10 @@
 
 set -e
 
-AWS_PROFILE=${1:-"ansilo"}
-
-if [[ -z "$AWS_PROFILE" ]];
-then
-    echo "Usage: $0 [aws profile=ansilo]"
-    exit 1
-fi
-
 docker build -t ansilo-devcontainer .
 
 docker tag ansilo-devcontainer 635198228996.dkr.ecr.ap-southeast-2.amazonaws.com/devcontainer:latest
 
-aws ecr get-login-password --profile $AWS_PROFILE --region ap-southeast-2 | docker login --username AWS --password-stdin 635198228996.dkr.ecr.ap-southeast-2.amazonaws.com
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 635198228996.dkr.ecr.ap-southeast-2.amazonaws.com
 
 docker push 635198228996.dkr.ecr.ap-southeast-2.amazonaws.com/devcontainer:latest
