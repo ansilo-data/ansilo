21b9b3a23b2837b99e62ccae428cdebbd5086b18
Elliot Levin <elliotlevin@hotmail.com>
2022-09-11 06:29:29 +0000

    fix strict function overloading

--- ansilo-pgx/src/util/strict.rs
+++ ansilo-pgx/src/util/strict.rs
@@ -13,7 +13,7 @@ CREATE FUNCTION "strict" (
 ) RETURNS bool
 LANGUAGE c /* Rust */
 COST 1
-AS 'MODULE_PATHNAME', 'strict_wrapper';
+AS 'MODULE_PATHNAME', 'strict_msg_wrapper';
 
 CREATE FUNCTION "strict" (
 	"flag" bool
@@ -26,8 +26,17 @@ AS 'MODULE_PATHNAME', 'strict_wrapper';
 );
 
 #[pg_extern(sql = "")]
-fn strict(flag: bool, message: Option<String>) -> bool {
-    if flag {
+fn strict(flag: Option<bool>) -> bool {
+    if flag == Some(true) {
+        return true;
+    }
+
+    error!("Strict check failed");
+}
+
+#[pg_extern(sql = "")]
+fn strict_msg(flag: Option<bool>, message: Option<String>) -> bool {
+    if flag == Some(true) {
         return true;
     }
 
