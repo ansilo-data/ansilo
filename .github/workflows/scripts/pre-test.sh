472341b3f9cf2a64abed65e9afe309ff876de01b
Elliot Levin <elliotlevin@hotmail.com>
2022-09-16 06:33:28 +0000

    install aws cli pre-test

--- .github/workflows/scripts/pre-test.sh
+++ .github/workflows/scripts/pre-test.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+echo ""
+echo "----- Installing aws cli -----"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 
+sudo unzip awscliv2.zip 
+sudo ./aws/install --update
+echo ""
+
 echo "----- Getting public ip -----"
 PUB_IP=$(curl https://ipinfo.io/json | jq -r .ip)
 echo "Public ip: $PUB_IP"
@@ -12,10 +19,4 @@ echo "----- Authorizing inbound from $PUB_IP -----"
 aws ec2 authorize-security-group-ingress \
     --group-id=sg-080dc71fb99e4fcb5 \
     --ip-permissions="IpProtocol=tcp,FromPort=0,ToPort=65535,IpRanges=[{CidrIp=$PUB_IP/32,Description='Authorise traffic from $PUB_IP@github-actions'}]"
-echo ""
-
-echo "----- Creating cache directories -----"
-mkdir -p ~/.cargo
-mkdir -p ~/.m2
-mkdir -p ~/target
 echo ""
