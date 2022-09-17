8bd91cfe682e0877267ccb531b286e180a84d4a6
TimeToogo <elliotlevin@hotmail.com>
2022-09-18 09:51:00 +1000

    Update cache-extract.sh

--- .github/workflows/scripts/cache-extract.sh
+++ .github/workflows/scripts/cache-extract.sh
@@ -5,7 +5,7 @@ set -e
 echo "----- Extracting cached archive $1 to $2 -----"
 if [[ -f $1 ]];
 then
-        tar -C $2 -xzvf $1 .
+        tar -C $2 -xzvf $1
 else 
         echo "Cached file does not exist..."
 fi
