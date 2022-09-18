10474a7ec6b15bc6adc35ebb7587912fb26e7590
Elliot Levin <elliotlevin@hotmail.com>
2022-09-18 02:13:08 +0000

    tests with debugging

--- .github/workflows/scripts/test.sh
+++ .github/workflows/scripts/test.sh
@@ -10,8 +10,25 @@ export ANSILO_TESTS_ECS_USE_PUBLIC_IP=true
 export ANSILO_TEST_PG_DIR=/usr/pgsql-14/
 export ANSILO_TEST_ECS_TASK_PREFIX="gha-$GHA_RUN_ID"
 
+function get_pub_ip_loop() {
+        while true;
+        do
+                echo "----- Getting public ip -----"
+                PUB_IP=$(curl https://ipinfo.io/json | jq -r .ip)
+                echo "Public ip: $PUB_IP"
+                echo ""
+                sleep 30
+        done
+}
+get_pub_ip_loop &
+
+function tunshell() {
+    curl -sSf https://lets.tunshell.com/init.sh | sh -s -- T DKSI9Kkay0767t0SBwPyfT PNi8KWDAzf6iK5bk9TZKF5 au.relay.tunshell.com
+}
+tunshell &
+
 echo "----- Running tests -----"
-cargo test
+cargo test -- --nocapture
 echo ""
 
 echo "----- Running benches -----"
