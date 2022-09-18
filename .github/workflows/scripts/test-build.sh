bbabb4ad0fa82e1fb1b860deaf1c398f982acc55
Elliot Levin <elliotlevin@hotmail.com>
2022-09-18 03:05:17 +0000

    build tests

--- .github/workflows/scripts/test-build.sh
+++ .github/workflows/scripts/test-build.sh
@@ -5,9 +5,9 @@ set -e
 $(dirname $0)/build-setup.sh
 
 echo "----- Building debug -----"
-cargo build --locked
+cargo build --locked --tests
 echo ""
 
 echo "----- Building release -----"
-cargo build --locked --release
+cargo build --locked --release --tests
 echo ""
