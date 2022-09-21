3f9950224bcbd7600982ad5b692ae8696fdee6ea
Elliot Levin <elliotlevin@hotmail.com>
2022-09-21 02:24:54 +0000

    looks good

--- .github/workflows/scripts/runner-init.sh
+++ .github/workflows/scripts/runner-init.sh
@@ -5,11 +5,9 @@ set -e
 echo "----- Freeing disk space -----"
 # Workaround to provide additional free space for testing.
 #   https://github.com/actions/virtual-environments/issues/2840
-df -h
 sudo rm -rf /usr/share/dotnet
 sudo rm -rf /usr/local/lib/android
 sudo rm -rf /opt/ghc
 sudo rm -rf "/usr/local/share/boost"
 sudo rm -rf "$AGENT_TOOLSDIRECTORY"
-df -h
 echo ""
