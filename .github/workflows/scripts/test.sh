094c0b7e54ab4a2b2bbc8fffe97269419ffbf2b0
Elliot Levin <elliotlevin@hotmail.com>
2022-09-25 14:45:41 +0000

    dont delete tmp files

--- .github/workflows/scripts/test.sh
+++ .github/workflows/scripts/test.sh
@@ -11,11 +11,10 @@ export ANSILO_TEST_PG_DIR=/usr/pgsql-14/
 export ANSILO_TEST_ECS_TASK_PREFIX="gha-$GHA_RUN_ID"
 export ANSILO_GHA_TESTS=true
 
-function clean_old_tmp_files() {
+function track_usage() {
     while true;
     do
-        echo "== Cleaning old /tmp files =="
-        find /tmp/ -type f -mmin +5 -delete >/dev/null 2>&1 || true
+        echo "== Track usage =="
         free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
         df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
         top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}' 
@@ -23,7 +22,7 @@ function clean_old_tmp_files() {
     done
 }
 
-clean_old_tmp_files &
+track_usage &
 
 echo "----- Running tests -----"
 cargo test
