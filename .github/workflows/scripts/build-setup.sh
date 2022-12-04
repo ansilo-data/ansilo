4ee0ea5dde9ba7efa4de0ee9e209b713aff42629
Elliot Levin <elliotlevin@hotmail.com>
2022-12-04 08:51:18 +0000

    fix pgx version in build

--- .github/workflows/scripts/build-setup.sh
+++ .github/workflows/scripts/build-setup.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "----- Installing cargo pgx -----"
-cargo install cargo-pgx --git https://github.com/tcdi/pgx.git --rev 4ad8a9
+cargo install cargo-pgx --version 0.6.0
 cargo pgx init --pg15 /usr/pgsql-15/bin/pg_config
 echo ""
